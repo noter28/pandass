f54d1688e8d50db4915db05ca95e91eac10b322f
noter28 <noterfield@gmail.com>
2018-11-19 17:28:33 +0200

    changed csv

--- pand.py
+++ pand.py
@@ -5,13 +5,21 @@ import sys
 df = pd.DataFrame({'id':range(15), 'chars':['ab']*15})
 print(df)
 '''
-df=pd.read_csv('qwerty.csv', sep=',')
+df1=pd.read_csv('df1.csv', sep=',')
+df2=pd.read_csv('df2.csv', sep=',')
+country = ['UA',u'RF',u'RF',u'RF',u'RF']
+df2.insert(1,'country',country)
+print(df2)
 
+
+
+
+'''
 print(df.columns)
 #print(df.dtypes)
 print(df.info)
 print(df.describe())
-'''
+
 new_line = {'data':25,'type':23}
 df=df.append(new_line, ignore_index=True)
 print(df.info)
