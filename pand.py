af4cc4d7dd5d1e52b541e87ad39ac7a96a6d01f8
noter28 <noterfield@gmail.com>
2018-11-19 16:20:19 +0200

    changed csv

--- pand.py
+++ pand.py
@@ -5,11 +5,11 @@ import sys
 df = pd.DataFrame({'id':range(15), 'chars':['ab']*15})
 print(df)
 '''
-df=pd.read_csv('eggs.csv',header=0, sep=',')
-'''
+df=pd.read_csv('qwerty.csv', sep=',')
+
 print(df.columns)
-print(df.dtypes)
-#print(df.info)
+#print(df.dtypes)
+print(df.info)
 print(df.describe())
 '''
 new_line = {'data':25,'type':23}
@@ -17,3 +17,4 @@ df=df.append(new_line, ignore_index=True)
 print(df.info)
 df=df.drop(['type'],axis=1)
 print(df)
+'''
