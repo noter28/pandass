24011b3d06f1fb11005e2361ab7ea5bd263e5ac3
noter28 <noterfield@gmail.com>
2018-11-19 15:55:12 +0200

    command

--- pand.py
+++ pand.py
@@ -1,1 +1,19 @@
-print(1)
+import pandas as pd
+import sys
+
+'''
+df = pd.DataFrame({'id':range(15), 'chars':['ab']*15})
+print(df)
+'''
+df=pd.read_csv('eggs.csv',header=0, sep=',')
+'''
+print(df.columns)
+print(df.dtypes)
+#print(df.info)
+print(df.describe())
+'''
+new_line = {'data':25,'type':23}
+df=df.append(new_line, ignore_index=True)
+print(df.info)
+df=df.drop(['type'],axis=1)
+print(df)
